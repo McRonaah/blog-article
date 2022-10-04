4c08fe886411dcc31c2b8206e085fbbd7f96bfca
McRonaah <ronaldcheruiyot342@gmail.com>
2022-10-04 09:23:20 +0300

    Create component updated

---
+++ my-app/src/components/Create.js
@@ -0,0 +1,26 @@
+
+const   Create = () =>{
+    return (
+        <div className="create">
+            <h1>Add a blog</h1>
+            <form>
+                <label>Blog Title</label>
+                <input
+                    type="text"
+                    required
+                />
+                <label>Blog Body</label>
+                <textarea
+                required
+                ></textarea> 
+                <label>Blog Author</label>
+                <select>
+                    <option value="ronald">ronald</option>
+                    <option value="dennis">dennis</option>
+                </select>
+                <button>Add New Blog</button>
+            </form>
+        </div>
+    );
+}
+
